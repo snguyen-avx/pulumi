da54d41ab62a6001e1041e011b772cc8f8fa51ab
snguyen <snguyen@aviatrix.com>
2024-06-21 07:35:37 -0500

    pre-compiled

---
+++ go-vpc-precompiled/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-awsx/sdk/go/awsx/ec2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+
+		vpc, err := ec2.NewVpc(ctx, "vpc-pulumi-go-precompiled", nil)
+		if err != nil {
+			return err
+		}
+
+		// Export a few properties to make them easy to use.
+		ctx.Export("vpcId", vpc.VpcId)
+		ctx.Export("privateSubnetIds", vpc.PrivateSubnetIds)
+		ctx.Export("publicSubnetIds", vpc.PublicSubnetIds)
+
+		return nil
+	})
+}
